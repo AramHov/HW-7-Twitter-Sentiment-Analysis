7ca6f3162763329c6512707a8f068862d0352b52
AramHov <hovasapyanaram@gmail.com>
2018-03-23 21:07:25 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+open_weather_key = "&APPID=3cebd113c22f5499d0952ab14b051d6c"
+
+open_weather_key_2 = "&APPID=90fd92f73d4a3e08c259f0e841f95e8b"
+
+
+
+consumer_key = "jCiriQ41RK64ab9oXgHabNwPp"
+consumer_secret = "TvS2Nv5U1sBHEGRXVopVlWZ2GYmLCuQQy8p86usTpPoyRkF4vf"
+access_token = "974457981751341056-s97fzzjtyhoerni3DK2M25H7JRiP9VZ"
+access_token_secret = "rlRiUCYUlSkQKqatkQTl9zO2ec0k2CSAi959pDNJHWd0s"
